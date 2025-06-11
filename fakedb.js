44da474f07cb6fb6f750800a2f1b3a8fa4243bde
unknown <sanmil>
2025-06-11 11:43:39 +0530

    removed fakeddb.json from gitignore

---
+++ fakedb.js
@@ -0,0 +1,32 @@
+export const users = [
+    {
+        id:'123',
+        firstName:'mukesh',
+        lastName:'kumar',
+        email:'mukesh@mukeshkumar.com',
+        password:'12345'
+    },
+    {
+        id:'3456',
+        firstName:'suresh',
+        lastName:'sharma',
+        email:'suresh@sureshsharma.com',
+        password:'1234553'
+    }
+]
+
+
+export const quotes = [
+    {
+        name:'I turn cofee into code',
+        by:'123'
+    },
+    {
+        name:'If it works dont touch it',
+        by:'3456'
+    },
+    {
+        name:'I am another quote',
+        by:'3456'
+    }
+]
